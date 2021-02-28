d4d9e3479508fa9804ad4ee40252f485f1d81f9e
TCKnight8 <sbwilliaowu@yahoo.com>
2021-02-28 15:54:52 -0600

    Bug removal
    The value in the first factorial was incorrectly written as the number 2. The variable N should be there, instead. This has been edited and corrected.

--- bi_calc.py
+++ bi_calc.py
@@ -1,7 +1,7 @@
 import scipy as sp
 
 def binomial_calc(x,N,pi):
-    prob = (sp.special.factorial(2)/
+    prob = (sp.special.factorial(N)/
            (sp.special.factorial(x)*
            (sp.special.factorial(N-x)))*
            (pi**x)*
